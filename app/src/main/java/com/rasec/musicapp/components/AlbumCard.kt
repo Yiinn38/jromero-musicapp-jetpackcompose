8910b3bd089c512f2668a4cb9b78bca61a7fcb21
Rasec Romero <rasec.romero46@gmail.com>
2025-10-15 17:41:24 -0600

    v5

--- app/src/main/java/com/rasec/musicapp/components/AlbumCard.kt
+++ app/src/main/java/com/rasec/musicapp/components/AlbumCard.kt
@@ -1,20 +1,21 @@
 package com.rasec.musicapp.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayCircle
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
@@ -23,7 +24,6 @@ import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.rasec.musicapp.models.Album
 import com.rasec.musicapp.ui.theme.DarkColor
-import com.rasec.musicapp.ui.theme.LightColor
 import com.rasec.musicapp.ui.theme.PrimaryColor
 
 @Composable
@@ -48,6 +48,17 @@ fun AlbumCard(
         contentScale = ContentScale.Crop,
         modifier = Modifier.fillMaxSize()
       )
+      Icon(
+        imageVector = Icons.Default.PlayCircle,
+        contentDescription = null,
+        tint = Color.White,
+        modifier = Modifier
+          .align(Alignment.BottomEnd)
+          .size(45.dp)
+          .padding(8.dp)
+          .alpha(0.7f)
+      )
+
     }
 
     Text(
